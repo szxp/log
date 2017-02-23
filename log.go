959db6c8380a0c52afaf6f682f3f049d78a8f9a3
Péter Szakszon <szp0530@gmail.com>
2017-02-23 18:54:07 +0100

    Update log.go

--- log.go
+++ log.go
@@ -1,3 +1,8 @@
+// Copyright 2017 Szakszon Péter. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+
 // Package log is a structured logging library.
 package log
 
